78888e73178b8312b284200c06073ae76d071c16
BranBer <brandonberke@gmail.com>
2021-07-11 18:08:08 -0400

    Add google login button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import FacebookLogin from 'react-facebook-login'
+import GoogleLogin from 'react-google-login';
 
 const responseFacebook = (response) => {
   console.log(response)
+}
+
+const responseGoogle = (response) => {
+  console.log(response);
 }
 
 function App() {
   return (
     <div className="App">
       <FacebookLogin
-        appId="489009555544014"
+        appId={process.env.FACEBOOK_CLIENT_ID}
         autoLoad={true}
         fields="name,firstName,lastName,location,email,picture"
         callback={responseFacebook} />
+
+      <GoogleLogin 
+        clientId={process.env.GOOGLE_CLIENT_ID}
+        buttonText="Login with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
